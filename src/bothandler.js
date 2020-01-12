099854a57a1b3df15d62319695a36a2d172a3bd9
mvoevodskiy <mv@aaa0.ru>
2020-01-13 02:48:44 +0300

    Init

---
+++ src/bothandler.js
@@ -0,0 +1,60 @@
+const BotCMS = require('botcms');
+const MVTools = require('mvtools');
+
+
+/**
+ * @class BotCMSHandler
+ *
+ * @property {Object} config
+ *
+ * @property {MvLoader} App
+ * @property {BotCMS} Bot
+ * @property {MVTools} MT
+ *
+ */
+class BotCMSHandler {
+    config = {};
+
+    constructor(App, config = {}) {
+        this.MT = new MVTools;
+        this.App = App;
+        this.loadConfig(config);
+        // console.log('BOTCMS HANDLER CONSTRUCTOR. CONFIG: ', this.config);
+    }
+
+    async init () {
+        // console.log('BOTCMS HANDLER INIT');
+        return Promise.resolve()
+            .then(() => {
+                let config = this.config.botcms || {};
+                config.db = this.MT.mergeRecursive(this.App.config.db, config.db);
+                this.Bot = new BotCMS(config);
+            })
+            .then(() => this.loadSchema(this.config.schemaFiles)
+            .then(() => this.Bot.init())
+            .then(async () => await this.Bot.launch()));
+            // .then(() => this.Bot.loadSchema('./overload.yml'))
+            // .then(() => console.log(this.Bot.Scripts.extract('c')))
+            // .then(() => this.TG = this.Bot.bridges.tg);
+    }
+
+    async loadConfig (config) {
+        this.config = this.MT.mergeRecursive(this.config, config);
+    }
+
+    async loadSchema (schemas) {
+        // let currentDir = __dirname + '/../';
+        // console.log('BOT HANDLER. LOAD SCHEMA. CURRENT DIR: ' + currentDir);
+        if (Array.isArray(schemas)) {
+            for (let schema of schemas) {
+                console.log('BOT HANDLER. LOAD SCHEMA. SCHEMA: ', schema);
+                await this.Bot.loadSchema(schema);
+            }
+        } else {
+            await this.Bot.loadSchema(schemas);
+        }
+    }
+
+}
+
+module.exports = BotCMSHandler;
