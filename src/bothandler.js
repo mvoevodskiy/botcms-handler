85655b9e674d545b533f62ab3635b36a1f0c3323
mvoevodskiy <mv@aaa0.ru>
2020-10-01 14:48:04 +0300

    Refactored by JS Standard Style

--- src/bothandler.js
+++ src/bothandler.js
@@ -1,69 +1,63 @@
-const BotCMS = require('botcms');
-const MVTools = require('mvtools');
-
+const BotCMS = require('botcms')
+const MVTools = require('mvtools')
 
 /**
  * @class BotCMSHandler
  *
- * @property {Object} config
- *
- * @property {MvLoader} App
+ * @property {Object<string, *>} config
+ * @property {import('mvloader').MVLoader} App
  * @property {BotCMS} Bot
  * @property {MVTools} MT
- *
  */
 class BotCMSHandler {
-    static exportConfig = {};
-    config = {};
-    defaults = {
-        botcms: {
-            db: {
-                name: 'botcms',
-            }
+  constructor (App, config = {}) {
+    this.config = {}
+    this.defaults = {
+      botcms: {
+        db: {
+          name: 'botcms'
         }
-    };
-
-    constructor(App, config = {}) {
-        this.MT = new MVTools;
-        this.App = App;
-        this.loadConfig(config);
-        // console.log('BOTCMS HANDLER CONSTRUCTOR. CONFIG: ', this.config);
+      }
     }
+    this.MT = new MVTools()
+    this.App = App
+    this.loadConfig(config)
+    // console.log('BOTCMS HANDLER CONSTRUCTOR. CONFIG: ', this.config);
+  }
 
-    async init () {
-        // console.log('BOTCMS HANDLER INIT');
-        return Promise.resolve()
-            .then(() => {
-                let config = this.config.botcms || {};
-                // config.db = this.MT.mergeRecursive(this.App.config.db, config.db);
-                this.Bot = new BotCMS(config);
-            })
-            .then(() => this.loadSchema(this.config.schemaFiles));
+  async init () {
+    // console.log('BOTCMS HANDLER INIT');
+    return Promise.resolve()
+      .then(() => {
+        const config = this.config.botcms || {}
+        // config.db = this.MT.mergeRecursive(this.App.config.db, config.db);
+        this.Bot = new BotCMS(config)
+      })
+      .then(() => this.loadSchema(this.config.schemaFiles))
+  }
+
+  async loadConfig (config) {
+    this.config = this.MT.mergeRecursive(this.defaults, this.config, config)
+  }
+
+  async initFinish () {
+    if (!this.MT.empty(this.App.DB)) {
+      this.Bot.DB = this.App.DB
     }
+    this.Bot.init().then(async () => await this.Bot.launch())
+  }
 
-    async loadConfig (config) {
-        this.config = this.MT.mergeRecursive(this.defaults, this.config, config);
+  async loadSchema (schemas) {
+    // console.log('BOT HANDLER. LOAD SCHEMA. SCHEMAS: ', schemas);
+    if (Array.isArray(schemas)) {
+      for (const schema of schemas) {
+        // console.log('BOT HANDLER. LOAD SCHEMA. SCHEMA: ', schema);
+        await this.Bot.loadSchema(schema)
+      }
+    } else {
+      await this.Bot.loadSchema(schemas)
     }
-
-    async initFinish () {
-        if (!this.MT.empty(this.App.DB)) {
-            this.Bot.DB = this.App.DB;
-        }
-        this.Bot.init().then(async () => await this.Bot.launch());
-    }
-
-    async loadSchema (schemas) {
-        // console.log('BOT HANDLER. LOAD SCHEMA. SCHEMAS: ', schemas);
-        if (Array.isArray(schemas)) {
-            for (let schema of schemas) {
-                // console.log('BOT HANDLER. LOAD SCHEMA. SCHEMA: ', schema);
-                await this.Bot.loadSchema(schema);
-            }
-        } else {
-            await this.Bot.loadSchema(schemas);
-        }
-    }
-
+  }
 }
 
-module.exports = BotCMSHandler;
+module.exports = BotCMSHandler
